78f5df605500f148394a9b09fc7a91ed63107c8b
Maciej Roszak <maciek.roszak08@gmail.com>
2025-11-03 08:33:50 +0000

    nic_niedziala_wysztko_lata

--- projekt02/index.js
+++ projekt02/index.js
@@ -1,22 +1,32 @@
 import express from "express";
+import flashcards from "./models/flashcards.js";
 
 const port = 8000;
 
-const app = express(); 
+const app = express();
 app.set("view engine", "ejs");
-app.use(express.static('./public'));
+app.use(express.static("public"));
 
-// app.get("/", (req, res) => { 
-//   res.set("Content-Type", "text/plain"); 
-//   res.send("Hello world"); 
-// });
+app.get("/cards", (req, res) => {
+  res.render("categories", {
+    title: "Kategorie",
+    categories: flashcards.getCategorySummaries(),
+  });
+});
 
-// app.get('/', (req, res) => {  
+app.get("/cards/:category_id", (req, res) => {
+  const category = flashcards.getCategory(req.params.category_id);
+  if (category != null) {
+    res.render("category", {
+      title: category.name,
+      category,
+    });
+  } else {
+    res.sendStatus(404);
+  }
+});
 
-//   res.render('index');
-// });
-
-
-app.listen(port, () => { 
+app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
+
